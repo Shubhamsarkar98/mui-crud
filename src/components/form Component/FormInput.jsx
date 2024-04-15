185979d11b5dbfb41706ac17e6ce6059001829bd
Sourabh-Choudhary7 <saurabh.bwn2001@gmail.com>
2024-04-15 15:24:07 +0530

    adding component

---
+++ src/components/form Component/FormInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextField } from '@mui/material'
+
+function FormInput({label}) {
+  return (
+    <>
+hello world
+        <TextField
+          label={label}
+          variant="outlined"
+        />
+    </>
+  )
+}
+
+export default FormInput
